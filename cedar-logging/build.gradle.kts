a81b6d0345d144b03a28c97e6928d6abdf0990d6
Merkost <kosta0212@gmail.com>
2025-07-11 10:47:13 +1000

    refactor: streamline iOS framework binary setup for CedarLogger

--- cedar-logging/build.gradle.kts
+++ cedar-logging/build.gradle.kts
@@ -9,9 +9,16 @@ kotlin {
 
     androidTarget { publishLibraryVariants("release") }
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach {
+        it.binaries.framework {
+            baseName = "CedarLogger"
+            isStatic = true
+        }
+    }
 
     sourceSets {
         commonMain.dependencies {
